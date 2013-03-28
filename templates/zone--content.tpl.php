bb212b2b2e177943e1120fa2661b1fe48605a7f5
Maks Mospanenko <max.invisible@gmail.com>
2013-03-29 00:53:11 +0200

    span* instead of grig- in breadcrumbs and messages

--- templates/zone--content.tpl.php
+++ templates/zone--content.tpl.php
@@ -1,10 +1,10 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>
   <div<?php print $content_attributes; ?>><div class="row">
     <?php if ($breadcrumb): ?>
-      <div id="breadcrumb" class="grid-<?php print $columns; ?>"><?php print $breadcrumb; ?></div>
+      <div id="breadcrumb" class="span<?php print $columns; ?>"><?php print $breadcrumb; ?></div>
     <?php endif; ?>    
     <?php if ($messages): ?>
-      <div id="messages" class="grid-<?php print $columns; ?>"><?php print $messages; ?></div>
+      <div id="messages" class="span<?php print $columns; ?>"><?php print $messages; ?></div>
     <?php endif; ?>
     <?php print $content; ?>
   </div></div>
